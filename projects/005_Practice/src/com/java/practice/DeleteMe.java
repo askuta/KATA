80dbd5691334dd960534b0aeeabeb03428da7a90
Zoltan Gabor <gaborzr@gmail.com>
2018-08-30 22:44:37 +0200

    Add project 005

---
+++ projects/005_Practice/src/com/java/practice/DeleteMe.java
@@ -0,0 +1,10 @@
+package com.java.practice;
+
+// Töröld ezt az osztályt!
+public class DeleteMe {
+
+	// Töröld ezt az osztályt!
+	public static void main(String[] args) {
+		// Töröld ezt az osztályt!
+	}
+}
